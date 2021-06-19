3cc90a81ce7f3d3956bda65fbb5ee35c3e1e5023
AmandaVV <amandavitorino397@gmail.com>
2021-06-19 14:36:31 -0300

    Arquivos da calculadora de imc

---
+++ fun.js
@@ -0,0 +1,12 @@
+const calcular = document.getElementById('calcular');
+const imc = document.getElementById('imc');
+const situacao = document.getElementById('situacao');
+const nome = document.getElementById('nome');
+const altura = document.getElementById('altura');
+const peso = document.getElementById('peso');
+
+function imc() {
+	alert ('teste click');
+}
+
+calcular.addEventListener('click', imc);
